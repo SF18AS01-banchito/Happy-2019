3d341c79f23ca4928536771eb262b2673fad3486
Esteban Ordonez <estebanusco@gmail.com>
2019-01-02 08:50:28 -0500

    Commit.

--- Happy 2019/ViewController.swift
+++ Happy 2019/ViewController.swift
@@ -9,10 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var lightOn : Bool = true;
 
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    @IBAction func swipeDown(_ sender: UISwipeGestureRecognizer) {
+        print("swipeDown");
+        lightOn = !lightOn;
+        mainLabel.text = "Welcome to 2019!";
+        updateUI();
+    }
+    
+    @IBAction func swipeUp(_ sender: UISwipeGestureRecognizer) {
+        print("swipeUp");
+        lightOn = !lightOn;
+        mainLabel.text = "Live long and prosper!";
+        updateUI();
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func updateUI(){
+        view.backgroundColor = lightOn ? .white : .black;
+        
     }
 
 
