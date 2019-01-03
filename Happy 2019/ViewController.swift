dd55a6c21aca3b0160dcfb3213c79fe25d466453
Esteban Ordonez <estebanusco@gmail.com>
2019-01-02 21:35:23 -0500

    App that tells you how many days until 12/31/2019.

--- Happy 2019/ViewController.swift
+++ Happy 2019/ViewController.swift
@@ -12,32 +12,77 @@ class ViewController: UIViewController {
     
     var lightOn : Bool = true;
 
+    @IBOutlet weak var beforeCount: UILabel!
     @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var askDate: UITextField!
+    @IBOutlet weak var afterCount: UILabel!
+    @IBOutlet weak var reallyAfterCount: UITextField!
     
-    @IBAction func swipeDown(_ sender: UISwipeGestureRecognizer) {
-        print("swipeDown");
-        lightOn = !lightOn;
-        mainLabel.text = "Welcome to 2019!";
-        updateUI();
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        askDate.isHidden = true;
+        date.isHidden = true;
+        beforeCount.isHidden = true;
     }
     
+    
+    @IBAction func senDate(_ sender: UITextField) {
+        sender.resignFirstResponder();
+        let d: Int = Int(askDate.text!)!;
+        let _: Int = d - (12312019);
+        //reallyAfterCount.text = "There are \(String(daysRemain)) to 2019";
+        
+    }
+    
+    @IBAction func swipe(_ sender: UISwipeGestureRecognizer) {
+        
+        lightOn = !lightOn;
+        updateUI();
+        
+        switch sender.direction.rawValue {
+            case 1:
+            mainLabel.text = "Welcome to 2019!";
+            askDate.isHidden = false;
+            date.isHidden = false;
+            beforeCount.isHidden = false;
+            case 2:
+            mainLabel.text = "Live long and prosper!";
+            askDate.isHidden = false;
+            date.isHidden = false;
+            beforeCount.isHidden = false;
+            case 4:
+            askDate.isHidden = false;
+            date.isHidden = false;
+            beforeCount.isHidden = false;
+            mainLabel.text = "There are "
+            case 8:
+            mainLabel.text = " Seize The Day ";
+            askDate.isHidden = false;
+            date.isHidden = false;
+            beforeCount.isHidden = false;
+            default:
+            mainLabel.text = " unreacognized direction ";
+            
+            }
+        
+        
+  /**
     @IBAction func swipeUp(_ sender: UISwipeGestureRecognizer) {
         print("swipeUp");
         lightOn = !lightOn;
         mainLabel.text = "Live long and prosper!";
         updateUI();
-        
+ **/
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+  
     
     func updateUI(){
         view.backgroundColor = lightOn ? .white : .black;
         
     }
-
+    
 
 }
 
